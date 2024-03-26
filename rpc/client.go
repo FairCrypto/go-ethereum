612f38eade68e6417e2ae8d457e27841e19f9c36
Nicholas Pettas <npettas@gmail.com>
2024-03-26 08:41:28 -0700

    return 503 on unhealthy response

--- rpc/client.go
+++ rpc/client.go
@@ -567,7 +567,7 @@ func (c *Client) dispatch(codec ServerCodec) {
 			if op.batch {
 				conn.handler.handleBatch(op.msgs)
 			} else {
-				conn.handler.handleMsg(op.msgs[0])
+				conn.handler.handleMsg(op.msgs[0], nil)
 			}
 
 		case err := <-c.readErr:
